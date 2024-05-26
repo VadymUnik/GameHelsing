e7ae92e7ada41c126cf0e1f80900cd7c847e2dc7
siriloid <9.ne.vadim@gmail.com>
2024-05-26 23:21:30 +0300

    added camera movement

--- GameHelsing/Assets/Scripts/CameraController.cs
+++ GameHelsing/Assets/Scripts/CameraController.cs
@@ -2,21 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
-    private float cameraHeight = -10f;
-    private GameObject Player;
-    private Vector3 setCameraPosition;
+    public Transform player;
 
-    void Start()
-    {
-        Player = GameObject.FindWithTag("Player");
+    Vector3 target, mousePos, refVel, shakeOffset;
 
+    float cameraDist = 3.5f;
+    float smoothTime = 0.2f, zStart; 
+    //shake
+    // float shakeMag, shakeTimeEnd;
+    
+    // Vector3 shakeVector;
+    bool shaking;
+
+    void Start(){
+        target = player.position;
+        zStart = transform.position.z;
     }
 
-    void Update()
-    {
-        setCameraPosition = new Vector3(Player.transform.position.x, Player.transform.position.y, cameraHeight);
-        transform.position = setCameraPosition;
+    void Update(){
+        mousePos = CaptureMousePos();
+    //    shakeOffset = UpdateShake();
+        target = UpdateTargetPos();
+        UpdateCameraPosition();
     }
+
+    //public void Shake (Vector3 direction, float magnitude, float length){
+    //    shaking = true;
+    //    shakeVector = direction;
+    //    shakeMag = magnitude; 
+    //    shakeTimeEnd = Time.time + length;
+    //}
+
+    //Vector3 UpdateShake(){
+    //    if (!shaking || Time.time > shakeTimeEnd){
+   //        shaking = false;
+    //        return Vector3.zero;
+    //    }
+    //    Vector3 tempOffset = shakeVector;
+   //     tempOffset *= shakeMag;
+    //    return tempOffset;
+   // }
+
+    Vector3 CaptureMousePos(){
+        Vector2 ret = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        ret *= 2;
+        ret -= Vector2.one;
+        float max = 0.9f;
+        if (Mathf.Abs(ret.x) > max || Mathf.Abs(ret.y) > max){
+            ret = ret.normalized;
+        }
+        return ret;
+    }
+
+    Vector3 UpdateTargetPos(){
+        Vector3 mouseOffset = mousePos * cameraDist;
+        Vector3 ret = player.position + mouseOffset;
+    //    ret += shakeOffset;
+        ret.z = zStart;
+        return ret;
+    }
+
+    void UpdateCameraPosition(){
+        Vector3 tempPos;
+        tempPos = Vector3.SmoothDamp(transform.position, target,
+                                    ref refVel, smoothTime);
+        transform.position = tempPos;
+    }
+
 }
--- GameHelsing/Assets/Scripts/CrosshairController.cs
+++ GameHelsing/Assets/Scripts/CrosshairController.cs
@@ -13,6 +13,7 @@ public class CrosshairController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         var screenPoint = Input.mousePosition;
         screenPoint.z = 10.0f; //distance of the plane from the camera
         transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
