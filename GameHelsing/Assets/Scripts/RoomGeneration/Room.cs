d1ad19fad14e690031ae7bf4eeb0662391f12a06
VadymUnik <168380752+VadymUnik@users.noreply.github.com>
2024-05-31 03:05:22 +0300

    Room gen finished!
    :D

---
+++ GameHelsing/Assets/Scripts/RoomGeneration/Room.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField] public Transform doorLeft, doorRight, doorBottom, doorTop;
+    [SerializeField] public Transform EntranceLeft, EntranceRight, EntranceBottom, EntranceTop;
+    [SerializeField] public Transform gateLeft, gateRight, gateBottom, gateTop;
+    [SerializeField] private float roomSize;
+
+    
+
+    public bool isLeftOpen = true, isRightOpen = true, isUpOpen = true, isDownOpen = true;
+    public Vector2 direction;
+    public Vector2 desiredDirection = new Vector2(0, 0);
+
+    public float GetRoomSize()
+    {
+        return roomSize;
+    }
+    public Vector2 GetDirection()
+    {
+        return direction;
+    }
+
+    public void SetDirection(Vector2Int direction)
+    {
+        this.direction = direction;
+    }
+
+    public bool CanCreateBranch()
+    {
+        bool canHorisontal;
+        bool canVertical;
+        Debug.Log(isLeftOpen + "   " + isRightOpen + "   " + isUpOpen + "   " + isDownOpen);
+        if (desiredDirection.x > 0)
+            canHorisontal = isLeftOpen;
+        else
+            canHorisontal = isRightOpen;
+
+        if (desiredDirection.y > 0)
+            canVertical = isUpOpen;
+        else
+            canVertical = isDownOpen;
+        
+        Debug.Log(canHorisontal + "" + canVertical);
+
+        return canHorisontal || canVertical;
+    }
+
+    public Vector2 GetDesiredDirection()
+    {
+        return desiredDirection;
+    }
+
+    public void SetRoomSize(float roomSize)
+    {
+        this.roomSize = roomSize;
+    }
+    public void SetDesiredDirection(Vector2 desiredDirection)
+    {
+        this.desiredDirection = desiredDirection;
+    }
+
+}
